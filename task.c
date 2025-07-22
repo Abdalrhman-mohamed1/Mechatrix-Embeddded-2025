790697d120ede7224c5cd9accb1b9b0960b9b55e
Abood <rakbdw@gmail.com>
2025-07-22 07:35:36 +0300

    Add files via upload

---
+++ task.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+int main(void)
+
+{
+    //     int x;
+    //     scanf("%d", &x);
+
+    //     int y;
+    //     scanf("%d", &y);
+    //     int rusult = ((x + y) * 3) - 10;
+    //     printf("%d\n", rusult);
+    //     return 0;
+    // TASK 1
+
+    // printf("Abdalrhman Mohamed Ahmed \n");
+    // printf("Faculty of (ai)-first year"); TASK2
+
+    // float celsius, fahrenheit;
+    // scanf("%f", &celsius);
+    // fahrenheit = (celsius * 9.0 / 5) + 32;
+    // printf("%f\n", fahrenheit);
+    // TASK 3
+
+    // char x;
+    // scanf("%c", &x);
+    // printf("%i\n", x);
+    // TASK 5
+
+    // int x, y, z;
+    // scanf("%d %d %d", &x, &y, &z);
+    // if (x > y && x > z)
+    // {
+    //     printf("%d \n", x);
+    // }
+    // if (y > x && y > z)
+    // {
+    //     printf("%d \n", y);
+    // }
+    // if (z > x && z > y)
+    // {
+    //     printf("%d \n", z);
+    // } task 6
+
+    // int x, y, z;
+    // scanf("%d %d %d", &x, &y, &z);
+    // if (x < y && x < z)
+    // {
+    //     printf("%d \n", x);
+    // }
+    // if (y < x && y < z)
+    // {
+    //     printf("%d \n", y);
+    // }
+    // if (z < x && z < y)
+    // {
+    //     printf("%d \n", z);
+    // } TASK 7
+
+    // int x;
+    // scanf("%d", &x);
+    // if (x >= 85)
+    // {
+    //     printf("Excellent\n");
+    // }
+    // else if (x >= 75)
+    // {
+    //     printf(" Very good\n");
+    // }
+    // else if (x >= 65)
+    //     printf("Good\n");
+    // else if (x >= 50)
+    // {
+    //     printf("Pass\n");
+    // }
+    // else if (x < 50)
+    // {
+    //     printf("Fail\n");
+    // } TASK 9
+
+    // printf("*\n");
+    // printf("**\n");
+    // printf("***\n");
+    // printf("****\n");
+    // printf("*****\n");
+    // TASK 12
+
+    // 2.4.8.10.11
+    return 0;
+}
